a8413d35f61fbf8d91c03e3720c91e98ebef1ab7
Parker Kemp <charles.kemp702@gmail.com>
2016-06-02 13:06:47 -0400

    Initial commit

---
+++ TestConnect/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("mc.spinalcraft.com");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8765);
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Connect(remoteEP);
+            byte[] message = Encoding.ASCII.GetBytes("Hello World!");
+            sock.Send(message);
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+        }
+    }
+}
