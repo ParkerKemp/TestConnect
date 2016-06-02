473c72f134186326a99e8c595eace545ea8f59e1
Parker Kemp <charles.kemp702@gmail.com>
2016-06-02 16:55:44 -0400

    Use command line arguments

--- TestConnect/Program.cs
+++ TestConnect/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,7 +7,37 @@ namespace TestConnect
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("TestConnect <address> <port> <message>");
+                return 1;
+            }
+            string address = args[0];
+            string message = args.Length > 2 ? args[2] : "Hello World!";
+            int port;
+            if(!int.TryParse(args[1], out port))
+            {
+                Console.WriteLine("TestConnect <address> <port> <message>");
+                return 1;
+            }
+
+            Socket sock = connectTo(address, port);
+
+            byte[] msgBytes = Encoding.ASCII.GetBytes(message);
+            sock.Send(msgBytes);
+
+            byte[] buffer = new byte[1024];
+            sock.Receive(buffer);
+            Console.WriteLine(System.Text.Encoding.Default.GetString(buffer));
+
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+            return 0;
+        }
+
+        static Socket connectTo(string address, int port)
         {
             IPHostEntry ipHostInfo = Dns.GetHostEntry("mc.spinalcraft.com");
             IPAddress ipAddress = ipHostInfo.AddressList[0];
@@ -14,10 +45,7 @@ namespace TestConnect
 
             Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             sock.Connect(remoteEP);
-            byte[] message = Encoding.ASCII.GetBytes("Hello World!");
-            sock.Send(message);
-            sock.Shutdown(SocketShutdown.Both);
-            sock.Close();
+            return sock;
         }
     }
 }
